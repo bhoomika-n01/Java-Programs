cd0b34fec199e60f491000bee2327d435aaa9199
Bhoomika N <gbhoomika18@gmail.com>
2025-08-27 15:52:14 +0530

    factorial of a number

---
+++ src/test/java/programs/FactorialOfANumber.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class FactorialOfANumber {
+
+	public static void main(String[] args) {
+		
+		int num = 8;
+		int fact = 1;
+		
+		for(int i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+		
+		System.out.println("Factorial of " + num + " is " + fact);
+	}
+
+}
