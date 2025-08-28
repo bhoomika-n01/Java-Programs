03ebc14b585d1e472a35670e6e4b57cc2edf2d3d
Bhoomika N <gbhoomika18@gmail.com>
2025-08-28 23:17:11 +0530

    reverse a number

---
+++ src/test/java/programs/ReverseANumber.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+		
+		int num = 12345;
+		int rev = 0;
+		
+		while(num != 0) {
+			int digit = num % 10;
+			rev = rev*10 + digit;
+			num = num / 10;
+		}
+		
+		System.out.println("reversed number: " + rev);
+
+	}
+
+}
