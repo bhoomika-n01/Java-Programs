aaf9f502b4b1df963ba989cf78719924bfe226b7
Bhoomika N <gbhoomika18@gmail.com>
2025-08-29 14:43:07 +0530

    checking armstrong number

---
+++ src/test/java/programs/CheckArmstrongNumber.java
@@ -0,0 +1,24 @@
+package programs;
+
+public class CheckArmstrongNumber {
+
+	public static void main(String[] args) {
+		
+		int num = 153;
+		int original = num;
+		int arms = 0;
+		
+		while(num != 0) {
+			int digit = num % 10;
+			arms = arms + digit*digit*digit;
+			num = num / 10;
+		}
+		
+		if (original == arms) {
+			System.out.println("is armstrong number");
+		}else {
+			System.out.println("is not armsstrong number");
+		}
+	}
+
+}
