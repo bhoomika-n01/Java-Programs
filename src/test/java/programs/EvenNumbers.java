dca94936288fbd42a3a0d77c21e3a3d182d9ecf8
Bhoomika N <gbhoomika18@gmail.com>
2025-08-29 20:40:55 +0530

    even numbers from 1 to n

---
+++ src/test/java/programs/EvenNumbers.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int n = 20;
+		
+		for(int i = 1; i <= n; i++) {
+			if (i % 2 == 0) {
+				System.out.print(i + ", ");
+			}
+		}
+
+	}
+
+}
