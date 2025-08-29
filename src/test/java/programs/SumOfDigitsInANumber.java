e79e1fd6ed43d7df8d8842af5ccd82df667fe1d8
Bhoomika N <gbhoomika18@gmail.com>
2025-08-29 20:44:48 +0530

    sum of digits in a number

---
+++ src/test/java/programs/SumOfDigitsInANumber.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class SumOfDigitsInANumber {
+
+	public static void main(String[] args) {
+		
+		int num = 123;
+		int sum = 0;
+		
+		while(num != 0) {
+			int digit = num % 10;
+			sum = sum + digit;
+			num = num / 10;
+		}
+		
+		System.out.println("Sum: " + sum);
+
+	}
+
+}
